1aeb7f6d74f5619288a712e8c56b3e1c709fa7e7
Zera Holladay <zholladay@wayfair.com>
2025-12-18 05:55:18 -0500

    Kubebuilder setup: fixed domain

--- internal/controller/gsmsecret_controller.go
+++ internal/controller/gsmsecret_controller.go
@@ -33,9 +33,9 @@ type GSMSecretReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=secrets.wayfair.com.wayfair.com,resources=gsmsecrets,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=secrets.wayfair.com.wayfair.com,resources=gsmsecrets/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=secrets.wayfair.com.wayfair.com,resources=gsmsecrets/finalizers,verbs=update
+// +kubebuilder:rbac:groups=secrets.wayfair.com,resources=gsmsecrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=secrets.wayfair.com,resources=gsmsecrets/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=secrets.wayfair.com,resources=gsmsecrets/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
